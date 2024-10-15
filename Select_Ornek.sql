c75fb5ce71229e228c1a3bb71ecbc5e056c7b399
sude <sudefidan@icloud.com>
2024-10-16 00:00:27 +0300

    select-ornek

---
+++ Select_Ornek.sql
@@ -0,0 +1,12 @@
+--Employess tablo içerisindeki FirstName-LastName-Title kolonlarını Listeleyelim
+select FirstName,LastName,Title from Employees
+
+--Products tablosu içerisindeki ProductName ve UnitPrice kolonlarını listeleyelim
+
+select 
+	ProductName,
+	UnitPrice
+from Products
+
+--Shippers tablosu içerisindeki tüm kolonları listeleyelim
+select * from Shippers
